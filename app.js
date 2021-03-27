edff0abe7194d5916d3511fb960cf1cd55fc7214
Vinícius Vieira <florianovinicius09@gmail.com>
2021-03-27 20:59:39 -0300

    tasfihgqwygrq

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'cad_usuarios'
+});
+
+conn.connect(function(err) {
+    if (err) throw err;
+        console.log("Conectado!");
+  });
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('/public', express.static(path.join(__dirname,'static')));
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, 'static', 'index.html'));
-})
+});
+
+
+app.post('/', (req, res) =>{
+    var test = document.getElementById('email').value;
+    console.log(test);
+    res.sendFile(path.join(__dirname, 'static', 'pag-inicial.html'));
+});
+
 app.listen(3000);
