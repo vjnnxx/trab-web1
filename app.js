594163bfe35a67c7c1fe19cf1a4765ddcd85b3f8
Vinícius Vieira <florianovinicius09@gmail.com>
2021-04-05 21:37:50 -0300

    Validação de formulário via backend implementada e refatoração

--- app.js
+++ app.js
@@ -1,28 +1,56 @@
-/*const express = require('express');
+const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use('/public', express.static(path.join(__dirname,'static')));
+app.use('/', express.static(path.join(__dirname,'static')));
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
 
+app.get('/cadastro', (req, res) =>{
+  res.sendFile(path.join(__dirname, 'static', 'cadastro.html'));
+});
 
 app.post('/', (req, res) =>{
     res.sendFile(path.join(__dirname, 'static', 'pag-inicial.html'));
+    const nome = req.body.nome;
 });
 
-app.listen(3000);*/
+app.post('/index.html', (req, res) =>{
+  
+  //Validar dados do form
 
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+  const { nome, email,  cpf, telefone, datanasc, senha, confsenha } = req.body;
+
+  console.log(req.body);
+
+  if(nome == "") {
+    console.log("O nome não pode ficar vazio!");
+    return res.redirect("/cadastro");
+  } if (senha !== confsenha) {
+    console.log("As senhas não coincidem!");
+     return res.redirect("/cadastro");
+  }if(senha == "" || confsenha == ""){
+    console.log("Os campos de senha não podem ficar vazios!")
+    return res.redirect("/cadastro");
+  } if (email == ""){
+    console.log("Preencha o campo de email corretamente!");
+    return res.redirect("/cadastro");
+  } if (cpf == ""){
+    console.log("Preencha o campo de CPF corretamente!");
+    return res.redirect("/cadastro");
+  } if(telefone == ""){
+    console.log("Preencha o campo de telefone corretamente!");
+    return res.redirect("/cadastro");
+  } if (datanasc == ""){
+    console.log("Preencha a data de nascimento corretamente!");
+    return res.redirect("/cadastro");
+  } else {
+    res.sendFile(path.join(__dirname, 'static', 'index.html'));
+  }
+});
+
+app.listen(3000);
