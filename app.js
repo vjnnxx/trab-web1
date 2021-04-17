9eaa22f47c9ecda22da8737edd306b9a7183b698
Vinícius Vieira <florianovinicius09@gmail.com>
2021-04-17 13:03:19 -0300

    Mudanças 17/04

--- app.js
+++ app.js
@@ -1,8 +1,73 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const app = express();
+
+//Funções
+const cadastraUsuarios = (nome, email, cpf, dataNasc, telefone, senha ) =>{
+  con.connect((err)=>{
+    if (err) throw err;
+    console.log('Conectado!')
+  });
+
+    let sql = "INSERT INTO usuarios (nome, email, cpf, dataNasc, telefone, senha) VALUES (?)";
+    let values = [[nome, email, cpf, dataNasc, telefone, senha]];
+      
+    con.query(sql, values, (err, result)=>{
+      if (err) throw err;
+      console.log("Numero de registros inseridos: " + result.affectedRows);
+    });
+    
+}
+
+const fazerLogin = async (email, senha) => {
+  con.connect( (err) => {
+    if (err) throw err;
+  });
+      
+  email = email.toString(); 
+    
+  //Query para buscar enviado dentro do bd
+  let sql = ("SELECT email from usuarios WHERE email = " + mysql.escape(email));
+  let regEmail, emailOk;
+  await con.query(sql, (err, result)=> { //Query para verificar se o email corresponde 
+    if (err) throw err;
+
+      regEmail = result[0].email;
+      
+      if(email == regEmail) {
+        emailOk = true;
+      } 
+  });
+  
+  let regSenha, senhaOk;
+  sql = ("SELECT senha from usuarios WHERE email = " + mysql.escape(email));
+  await con.query(sql, (err, result)=>{
+    if (err) throw err;
+        
+    regSenha = result[0].senha; 
+        
+    //console.log(senha, regSenha);
+        
+    bcrypt.compare(senha, regSenha, (err, result)=>{
+      if (err) throw err;
+        
+      if(result){
+        senhaOk = true;
+      }
+    });
+  });
+ 
+  
+  console.log(emailOk, senhaOk);
+  let confLogin = false;
+  if(senhaOk && emailOk){
+    confLogin = true;
+  }
+  return confLogin;
+}
 
 //Estabelece conexão com o banco de dados
 const con = mysql.createConnection({
@@ -14,20 +79,33 @@ const con = mysql.createConnection({
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', express.static(path.join(__dirname,'static')));
+
+//Path raiz
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
 
+//Path da pagina de cadastro
 app.get('/cadastro', (req, res) =>{
   res.sendFile(path.join(__dirname, 'static', 'cadastro.html'));
 });
 
-app.post('/', (req, res) =>{
+//Path para fazer login no site
+app.post('/',  async (req, res) =>{
+  let email = req.body.email;
+  let senha = req.body.senha;
+
+  let confLog = fazerLogin(email, senha);
+  console.log("Buiu: " + confLog);
+  if(confLog){
     res.sendFile(path.join(__dirname, 'static', 'pag-inicial.html'));
-    const nome = req.body.nome;
+  } else {
+    res.redirect('/');
+  }
 });
 
-app.post('/index.html', (req, res) =>{
+//Path para criar registro no sistema
+app.post('/index.html', async (req, res) =>{
   
   //Validar dados do form
 
@@ -64,30 +142,19 @@ app.post('/index.html', (req, res) =>{
     let telefoneForm = telefone.toString();
     let datanascForm = datanasc.toString();
     let senhaForm = senha.toString();
-    let confsenhaForm = senha.toString();
+
+    //Encripta a senha 
+    let senhaEncriptada = await bcrypt.hash(senhaForm, 10);
+  
 
     // Remove caracteres inválidos dos valores
     cpfForm = cpf.replace(/[^0-9]/g, '');
     telefoneForm = telefone.replace(/[^0-9]/g, '');
 
     //Insere valores no banco de dados 
-    con.connect((err)=>{
-      if (err) throw err;
-      console.log('Conectado!')
-      let sql = "INSERT INTO usuarios (nome, email, cpf, dataNasc, telefone, senha) VALUES (?)";
-      let values = [[nomeForm, emailForm, cpfForm, datanascForm, telefoneForm, senhaForm]];
-      
-      
+    
+    cadastraUsuarios(nomeForm, emailForm, cpfForm, datanascForm, telefoneForm, senhaEncriptada);
 
-      con.query(sql, values, (err, result)=>{
-        if (err) throw err;
-        console.log("Numero de registros inseridos: " + result.affectedRows);
-      });
-      
-    });
-
-
-    //Redireciona o usuario para a pagina inicial
     res.sendFile(path.join(__dirname, 'static', 'index.html'));
   }
 });
